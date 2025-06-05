ba8230e0194d46081714cbad7e2ee1f05755f36e
Богачев Матвей <boga4iov.matvei2016@yandex.ru>
2025-06-06 00:42:54 +0400

    User DTO

---
+++ EasyWish.Domain/Dto/UserDto/PostUser.cs
@@ -0,0 +1,14 @@
+﻿namespace EasyWish.Domain.Dto.UserDto;
+
+public class PostUser
+{
+
+    public string? UserName { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+}
---
+++ EasyWish.Domain/Dto/UserDto/PutUser.cs
@@ -0,0 +1,18 @@
+﻿using EasyWish.Domain.Class;
+
+namespace EasyWish.Domain.Dto.UserDto;
+
+public class PutUser
+{
+    public string? UserName { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public ICollection<WishList>? Lists { get; set; }
+
+    public ICollection<Friendship>? Friends { get; set; }
+}
