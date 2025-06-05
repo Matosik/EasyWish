de2c3910bdb226f90246f6b1cdb1b288cef598fd
Богачев Матвей <boga4iov.matvei2016@yandex.ru>
2025-06-06 01:14:46 +0400

    добавлены в DI Mapper, репозиторий, DbContext

---
+++ EasyWish.WebServerAPI/Program.cs
@@ -0,0 +1,32 @@
+using EasyWish.Domain;
+using EasyWish.Domain.Repository;
+using EasyWish.Domain.Tools;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<EasyWishContext>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddAutoMapper(typeof(Mapper));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
