d70d77e84e37c593b3dcab6ffb7ca42b329a3327
Богачев Матвей <boga4iov.matvei2016@yandex.ru>
2025-06-06 00:43:16 +0400

    Mapper for User Dto

---
+++ EasyWish.Domain/Tools/Mapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using EasyWish.Domain.Class;
+using EasyWish.Domain.Dto.UserDto;
+
+
+namespace EasyWish.Domain.Tools;
+
+class Mapper : Profile
+{
+    public Mapper()
+    {
+        CreateMap<User, GetUser>().ReverseMap();
+        CreateMap<User, PostUser>().ReverseMap();
+        CreateMap<User, PutUser>().ReverseMap();
+
+    }
+}
